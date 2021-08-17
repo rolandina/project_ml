cd48383d08eecf24c99d0134f07ea0241f2eb96d
Smirnova Nina <ms.nina.smirnova@gmail.com>
2021-08-17 23:59:53 +0200

    js added

--- front-end/script.js
+++ front-end/script.js
@@ -1,3 +1,42 @@
 function rangeSlide(value) {
     document.getElementById('rangeValue').innerHTML = value;
 }
+
+$(document).ready(() => {
+
+    $('#result').hide()
+    $('form').on('submit', event => {
+    
+        event.preventDefault();
+    
+        var sibsp =$("#sibsp").val();
+        var age =$("#age").val();
+        var sex =$("#sex").val();
+        var fare = $("#fare").val();
+        var pclass =$("#pclass").val();
+        var embarked =$("#embarked").val();
+    
+        url =`https://titanicfastapi.azurewebsites.net/prediction/?pclass=${pclass}&sex=${sex}&age=${age}&sibsp=${sibsp}&fare=${fare}&embarked=${embarked}`;
+        
+        // fetch(url)
+        // .then(response => response.json())
+        // .then(data =>
+        //     console.log(data)
+        // ) 
+    
+        $.ajax({
+            url: url, 
+            type: "GET", 
+            dataType: "json",
+        })
+        .done(function (json) {
+            text = json['description']
+            console.log(text)
+            $('#result').text(text)
+            $('#result').fadeIn()
+        })
+    
+    })
+    
+    });
+    
